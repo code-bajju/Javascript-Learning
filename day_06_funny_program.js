a74675da7e27c1f4c2e8435b6ac01a897aa7cfdf
Bajrang X code.bajju <104210183+code-bajju@users.noreply.github.com>
2023-03-10 23:11:51 +0530

    Funny Program

---
+++ day_06_funny_program.js
@@ -0,0 +1,22 @@
+let name = "code bajju";
+
+console.log("Welcome to " + name + "!");
+console.log("Get ready to laugh!");
+
+let laughter = "";
+
+// loop to add "ha" to laughter variable
+for (let i = 0; i < 10; i++) {
+  laughter += "ha";
+}
+
+console.log(laughter + "!");
+console.log("Did you hear about the programmer who got stuck in the shower? He kept trying to wash his backslash!");
+console.log(laughter + "!");
+console.log("Why do programmers prefer dark mode? Because light attracts bugs!");
+console.log(laughter + "!");
+console.log("Why do programmers wear glasses? Because they can't C#!");
+console.log(laughter + "!");
+console.log("Why did the programmer quit his job? He didn't get arrays!");
+console.log(laughter + "!");
+console.log("Thanks for joining " + name + "! Have a hilarious day!");
